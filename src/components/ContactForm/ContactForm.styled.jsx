f12cf0e27c81fa408f468a13ee559d2bce8b1aea
ShcherbatayaLuna <105154403+ShcherbatayaLuna@users.noreply.github.com>
2022-11-26 23:56:54 +0200

    Update ContactForm

--- src/components/ContactForm/ContactForm.styled.jsx
+++ src/components/ContactForm/ContactForm.styled.jsx
@@ -1,11 +1,13 @@
 import styled from 'styled-components';
 
 export const Form = styled.form`
+  margin-left: auto;
+  margin-right: auto;
   display: flex;
   flex-direction: column;
   width: 400px;
   padding: 20px;
-  border: 2px solid black;
+  border: 2px solid #1976d2;
   margin-bottom: 20px;
 `;
 
@@ -16,11 +18,14 @@ export const Label = styled.label`
 export const Input = styled.input`
   width: 250px;
   padding: 8px;
+  border: 1px solid lightgray;
+  border-radius: 4px;
   margin-bottom: 20px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15);
 `;
 
 export const Button = styled.button`
-   display: flex;
+  display: flex;
   justify-content: center;
   align-items: center;
 
@@ -30,12 +35,13 @@ export const Button = styled.button`
   border-radius: 4px;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15);
 
-  background-color: white;
+  background-color: #ffffff;
   cursor: pointer;
 
   :hover,
   :focus {
-    border: 1px solid #9a20bc;
-    background-color: #e589ff;
+    color: #ffffff;
+    border: 1px solid #1976d2;
+    background-color: #1976d2;
   }
 `;
