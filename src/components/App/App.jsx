7f5e774bb3c460b2577fae8d37d89f83245a34ab
ShcherbatayaLuna <105154403+ShcherbatayaLuna@users.noreply.github.com>
2022-11-26 13:21:34 +0200

    Update App

---
+++ src/components/App/App.jsx
@@ -0,0 +1,52 @@
+import { lazy, Suspense, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { refreshUser } from 'redux/auth/authOperations';
+import PublicRoute from '../PublicRoute';
+import PrivateRoute from '../PrivateRoute';
+import { Container } from './App.styled';
+
+const Home = lazy(() => import('../pages/Home'));
+const Register = lazy(() => import('../pages/Register'));
+const Login = lazy(() => import('../pages/Login'));
+const Contacts = lazy(() => import('../pages/Contacts/Contacts'));
+const Layout = lazy(() => import('../pages/Layout'));
+
+export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
+
+  return (
+    <Container>
+      <Suspense fallback={null}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route
+              path="/register"
+              element={
+                <PublicRoute redirectTo="/contacts" component={<Register />} />
+              }
+            />
+            <Route
+              path="/login"
+              element={
+                <PublicRoute redirectTo="/contacts" component={<Login />} />
+              }
+            />
+            <Route
+              path="/contacts"
+              element={
+                <PrivateRoute redirectTo="/login" component={<Contacts />} />
+              }
+            />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Route>
+        </Routes>
+      </Suspense>
+    </Container>
+  );
+}
