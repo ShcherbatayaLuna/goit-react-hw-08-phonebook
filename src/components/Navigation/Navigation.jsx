31c0456560b28cda4463dddb24376f48f42a7309
ShcherbatayaLuna <105154403+ShcherbatayaLuna@users.noreply.github.com>
2022-11-26 23:57:16 +0200

    Upadte Navigation

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,14 +1,14 @@
-import { NavLink } from 'react-router-dom';
+import { Box, Button, Link } from './Navigation.styled';
 
 export default function Navigation() {
   return (
-    <div>
-      <NavLink to="/register">
-        <button type="button">Register</button>
-      </NavLink>
-      <NavLink to="/login">
-        <button type="button">Log In</button>
-      </NavLink>
-    </div>
+    <Box>
+      <Button type="button">
+        <Link to="/register">Register</Link>
+      </Button>
+      <Button type="button">
+        <Link to="/login">Log in</Link>
+      </Button>
+    </Box>
   );
 }
